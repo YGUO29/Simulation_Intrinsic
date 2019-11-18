8727cfaff036311730f885d1abf81431cc88d24a
Yueqi <yguo29@jhu.edu>
2019-11-18 15:00:50 -0500

    new para

--- mcxlab-win-x86_64-v2019.4/mcxlab/XINTRINSIC_SIM.m
+++ mcxlab-win-x86_64-v2019.4/mcxlab/XINTRINSIC_SIM.m
@@ -6,37 +6,6 @@
 % This file is part of Monte Carlo eXtreme (MCX) URL:http://mcx.sf.net
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear cfg;
-%% set tissue parameters
-load('D:\=code=\Simulation_Intrinsic\spectra.mat')
-lambda = spectra(:,1); e_HbO = spectra(:,2); e_HbR = spectra(:,3); 
-c_Hb   = 129; % hemoglobin (g/L) used in Hilman 2016
-perc_blood = 0.03; % blood volume / tissue volume
-sat_O2 = 0.75;
-c_HbO = c_Hb*sat_O2; c_HbR = c_Hb*(1-sat_O2);
-mua_HbR = perc_blood.*(e_HbR.*c_HbR.*2.303./64500)./10; % unitsL 1/mm
-mua_HbO = perc_blood.*(e_HbO.*c_HbO.*2.303./64500)./10;
-mua_HbT = mua_HbO+mua_HbR;
-
-WLs = [470 530 590 625 730 850];
-ind = floor(interp1(lambda,1:length(lambda), WLs));
-
-figure('DefaultAxesFontSize',18, 'DefaultLineLineWidth', 2),
-
-subplot(121), hold on, set(gca, 'YScale', 'log')
-semilogy(lambda, mua_HbR), semilogy(spectra(:,1),mua_HbO),
-semilogy(lambda,mua_HbT,'k')
-scatter(lambda(ind),mua_HbT(ind),32)
-for i = 1:length(ind)
-text(lambda(ind(i))+5,mua_HbT(ind(i)),num2str(mua_HbT(ind(i)), 2))
-end
-title('Absorption coefficient')
-legend('mua-HbR', 'mua-HbO2', 'mua-HbT')
-subplot(122), hold on, set(gca, 'YScale', 'log')
-semilogy(spectra(:,1),e_HbR), hold on, semilogy(spectra(:,1),e_HbO)
-for i = 1:length(ind); xline(lambda(ind(i)),'--'); end
-title('Moler extinction coefficient')
-legend('\epsilon-HbR', '\epsilon-HbO2')
-
 
 %% preparing the input data
 
--- mcxlab-win-x86_64-v2019.4/mcxlab/XINTRINSIC_SIM_SkullThickness.m
+++ mcxlab-win-x86_64-v2019.4/mcxlab/XINTRINSIC_SIM_SkullThickness.m
@@ -7,43 +7,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear cfg;
 %% set tissue parameters
-load('C:\Users\guoyu\Documents\MATLAB\Simulation_Intrinsic\spectra.mat')
-lambda = spectra(:,1); e_HbO = spectra(:,2); e_HbR = spectra(:,3); 
-c_Hb   = 129; % hemoglobin (g/L) used in Hilman 2016
-perc_blood = 0.03; % blood volume / tissue volume
-sat_O2 = 0.75;
-c_HbO = c_Hb*sat_O2; c_HbR = c_Hb*(1-sat_O2);
-mua_HbR = perc_blood.*(e_HbR.*c_HbR.*2.303./64500)./10; % unitsL 1/mm
-mua_HbO = perc_blood.*(e_HbO.*c_HbO.*2.303./64500)./10;
-mua_HbT = mua_HbO+mua_HbR;
-
-WLs = [470 530 590 625 730 850];
-ind = floor(interp1(lambda,1:length(lambda), WLs));
-
-figure('DefaultAxesFontSize',18, 'DefaultLineLineWidth', 2),
-subplot(121), hold on, set(gca, 'YScale', 'log')
-semilogy(lambda, mua_HbR), semilogy(spectra(:,1),mua_HbO),
-semilogy(lambda,mua_HbT,'k')
-scatter(lambda(ind),mua_HbT(ind),32)
-for i = 1:length(ind)
-text(lambda(ind(i))+5,mua_HbT(ind(i)),num2str(mua_HbT(ind(i)), 2))
+load('C:\Users\guoyu\Documents\MATLAB\Simulation_Intrinsic\optical_para.mat')
+for i = 1:length(optical_para_label)
+    eval([optical_para_label{i}, '= optical_para(:,i);']);
 end
-title('Absorption coefficient')
-legend('mua-HbR', 'mua-HbO2', 'mua-HbT')
-subplot(122), hold on, set(gca, 'YScale', 'log')
-semilogy(spectra(:,1),e_HbR), hold on, semilogy(spectra(:,1),e_HbO)
-for i = 1:length(ind); xline(lambda(ind(i)),'--'); end
-title('Moler extinction coefficient')
-legend('\epsilon-HbR', '\epsilon-HbO2')
-
-
-load('C:\Users\guoyu\Documents\MATLAB\Simulation_Intrinsic\Skull_mus.mat')
-g_skull = 0.89;
-lambda_skull = Skull_mus(:,1);
-musr_skull = Skull_mus(:,2);
-figure('DefaultAxesFontSize',18, 'DefaultLineLineWidth', 2),
-plot(lambda_skull,musr_skull)
-mus_skull = interp1(lambda_skull,musr_skull,WLs,'linear','extrap')./(1-g_skull);
 %% preparing the input data
 
     % set seed to make the simulation repeatible
